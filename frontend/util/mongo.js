58231c822a1252150d59d7c1dbcb402ef28282b0
EienMosu <voodoochild9293@hotmail.com>
2022-03-04 22:20:12 +0100

    moongose models added

---
+++ frontend/util/mongo.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+const MONGODB_URL = process.env.MONGODB_URL
+
+if (!MONGODB_URL) {
+  throw new Error(
+    'Please define the MONGODB_URL environment variable inside .env.local'
+  )
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    }
+
+    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
+      return mongoose
+    })
+  }
+  cached.conn = await cached.promise
+  return cached.conn
+}
+
+export default dbConnect
