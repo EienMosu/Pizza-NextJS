380176aad245c7060d18cd884025db23fd190845
EienMosu <voodoochild9293@hotmail.com>
2022-03-06 05:52:21 +0100

    MongoDB is added to project succesfully

--- frontend/pages/product/[id].jsx
+++ frontend/pages/product/[id].jsx
@@ -5,26 +5,47 @@ import axios from "axios";
 
 const Product = ({ pizza }) => {
   const [size, setSize] = useState(0);
-  const [price, setPrice] = useState(pizza.prices[size]);
-  const [add, setAdd] = useState(0);
+  const [price, setPrice] = useState(pizza.prices[0]);
+  const [extras, setExtras] = useState([]);
   const [quantity, setQuantity] = useState(1);
 
-  const handleExtraPrice = (event, index) => {
-    console.log(pizza.extraOptions[index]?.text)
-
-    if (event.target.checked) {
-      pizza.extraOptions[index]?.text === event.target.name &&
-        setAdd(pizza.extraOptions?.price);
-    } else if(!event.target.checkbox) {
-      return console.log("Dsadsa")
-    }
+  const changePrice = (number) => {
+    setPrice(price + number);
   };
 
-  useEffect(() => {
-    const finalPrice = quantity * pizza.prices[size];
+  const handleSize = (sizeIndex) => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size];
 
-    setPrice(finalPrice);
-  }, [quantity]);
+    setSize(sizeIndex);
+
+    changePrice(difference);
+  };
+
+  const handleChange = (event, option) => {
+    const checked = event.target.checked;
+
+    if (checked) {
+      changePrice(option.price);
+
+      setExtras((prev) => [...prev, option]);
+
+      //or
+      // setExtras([...extras, option]);
+
+      // Another way of the add
+      // !extras.includes(option) && extras.push(option);
+    } else {
+      changePrice(-option.price);
+
+      setExtras(extras.filter((extra) => extra._id !== option._id));
+
+      // Another way of the remove
+      // const index = extras.indexOf(option);
+      // if (index > -1) {
+      //   extras.splice(index, 1);
+      // }
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -39,29 +60,29 @@ const Product = ({ pizza }) => {
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
-          <div className={styles.size} onClick={() => setSize(0)}>
+          <div className={styles.size} onClick={() => handleSize(0)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <span className={styles.number}>Small</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(1)}>
+          <div className={styles.size} onClick={() => handleSize(1)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <span className={styles.number}>Medium</span>
           </div>
-          <div className={styles.size} onClick={() => setSize(2)}>
+          <div className={styles.size} onClick={() => handleSize(2)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <span className={styles.number}>Large</span>
           </div>
         </div>
         <h3 className={styles.choose}>Choose additinional ingredients</h3>
         <div className={styles.ingredients}>
-          {pizza.extraOptions?.map((options, index) => (
+          {pizza.extraOptions?.map((options) => (
             <div className={styles.option} key={options._id}>
               <input
                 type="checkbox"
                 id={options.text}
                 name={options.text}
                 className={styles.checkbox}
-                onClick={(event) => handleExtraPrice(event, index)}
+                onChange={(event) => handleChange(event, options)}
               />
               <label htmlFor="double">{options.text}</label>
             </div>
@@ -70,9 +91,11 @@ const Product = ({ pizza }) => {
         <div className={styles.add}>
           <input
             type="number"
-            value={quantity}
-            onChange={(event) => setQuantity(event.target.value)}
             className={styles.quantity}
+            defaultValue="1"
+            min="1"
+            max="10"
+            onChange={(event) => setQuantity(event.target.value)}
           />
           <button className={styles.button}>Add to Cart</button>
         </div>
